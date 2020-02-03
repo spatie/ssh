de1160490a15f1118956b7a5eb8c7b2c6d492659
Freek Van der Herten <freek@spatie.be>
2020-02-03 13:21:13 +0000

    Apply fixes from StyleCI

--- tests/__snapshots__/SshTest__it_can_use_a_specific_public_key__1.php
+++ tests/__snapshots__/SshTest__it_can_use_a_specific_public_key__1.php
@@ -1,3 +1,5 @@
-<?php return 'ssh -i /home/user/.ssh/id_rsa user@example.com \'bash -se\' << \\EOF-SPATIE-SSH
+<?php
+
+return 'ssh -i /home/user/.ssh/id_rsa user@example.com \'bash -se\' << \\EOF-SPATIE-SSH
 whoami
 EOF-SPATIE-SSH';
